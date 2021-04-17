ec8a5b8a324bf47da4c680971508cd4348451b44
Carson <rodriguescarson@hotmail.com>
2021-04-18 01:08:33 +0530

    Update config.js

--- crio.do-portfolio-main/profile-frontend/src/data/config.js
+++ crio.do-portfolio-main/profile-frontend/src/data/config.js
@@ -1,9 +1,9 @@
 
 module.exports = {
   // TODO: change your profile information here
-  name: "Carson Rodrigues aa",
+  name: "Carson Rodrigues",
   greeting: "Hey 👋",
-  greetingDescription: "I'm Carson Rodrigues and I'm a Computer Engineering Student!",
+  greetingDescription: "I'm Carson Rodrigues and I'm a Computer Engineering Student!!!",
   githubUrl: "https://github.com/rodriguescarson",
   linkedinUrl: "https://linkedin.com/in/rodriguescarson",
   cvLink: "https://drive.google.com/file/d/1_g-k3T3Izt3iTDFmqGmN-Kmx5X4zcJnf/view?usp=sharing",
